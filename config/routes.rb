b81a3684b801729f0f322c78a25c849481bc5963
Joseph Mindo <mindo.joseph.mj@gmail.com>
2021-01-21 18:46:04 +0300

    Link to trainers page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'sessions/show'
-  resources :courses, :uploads
+  resources :courses, :uploads, :trainers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
   get '/sessions', to: 'sessions#show'
