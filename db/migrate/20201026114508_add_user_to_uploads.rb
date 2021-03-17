838e10afaf47c0b0685a744899c652a975bb74d8
Mindo-Joseph <mindo.joseph.mj@gmail.com>
2021-03-17 13:47:22 +0300

    Fix typo in migration file

--- db/migrate/20201026114508_add_user_to_uploads.rb
+++ db/migrate/20201026114508_add_user_to_uploads.rb
@@ -1,5 +1,5 @@
 class AddUserToUploads < ActiveRecord::Migration[6.0]
   def change
-    add_reference :uploads, :user, foreign_key: true
+    add_reference :uploads, :users, foreign_key: true
   end
 end
